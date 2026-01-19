3b09d79ff25ad0f4f2b2d4abcd7090c8f0eb5dbb
Aditi Baghel <120701036+aditi644@users.noreply.github.com>
2026-01-19 11:59:36 +0530

    Clean up index.js by removing unused code
    Removed unnecessary imports and middleware setup from index.js.

--- backend/index.js
+++ backend/index.js
@@ -40,45 +40,5 @@ server.listen(PORT, () => {
     connectDB();
 });
 
-import express from 'express';
-import authRoutes from './src/routes/auth.route.js'
-import messageRoutes from './src/routes/message.route.js'
-import invoiceRoutes from './src/routes/invoices.route.js'
-import dotenv from 'dotenv'
-import { connectDB } from './src/lib/db.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
-import { app, server } from './src/lib/socket.js';
-import path from "path"
 
-dotenv.config({ path: "../.env" });
-
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}))
-
-const PORT = process.env.PORT;
-const _dirname = path.resolve();
-
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
-app.use("/api/invoices", invoiceRoutes)
-
-if (process.env.NODE_ENV == "production"){
-    app.use(express.static(path.join(_dirname, "../frontend/dist")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
-    })
-}
-
-server.listen(PORT, () => {
-    console.log("Server is running on port PORT:" + PORT);
-    connectDB();
-
-});
 
