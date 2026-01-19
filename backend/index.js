fca9722647bb4b7542d27197df61f3a54d073877
Aditi Baghel <120701036+aditi644@users.noreply.github.com>
2026-01-19 10:31:17 +0530

    Refactor import paths in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
-import invoiceRoutes from './routes/invoices.route.js'
+import authRoutes from './src/routes/auth.route.js'
+import messageRoutes from './src/routes/message.route.js'
+import invoiceRoutes from './src/routes/invoices.route.js'
 import dotenv from 'dotenv'
-import { connectDB } from './lib/db.js';
+import { connectDB } from './src/lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
-import { app, server } from './lib/socket.js';
+import { app, server } from './src/lib/socket.js';
 import path from "path"
 
 dotenv.config({ path: "../.env" });
@@ -37,4 +37,5 @@ if (process.env.NODE_ENV == "production"){
 server.listen(PORT, () => {
     console.log("Server is running on port PORT:" + PORT);
     connectDB();
+
 });
