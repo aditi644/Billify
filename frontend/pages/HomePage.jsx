0e18a7b898f29173b2c02a37a25038cfbc4924aa
Aditi Baghel <120701036+aditi644@users.noreply.github.com>
2026-01-19 11:40:39 +0530

    Remove unused chat store and component imports
    Removed unused imports related to chat functionality.

--- frontend/pages/HomePage.jsx
+++ frontend/pages/HomePage.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { useChatStore } from '../src/store/useChatStore'
-import Sidebar from "../components/Sidebar";
-import NoChatSelected from "../components/NoChatSelected";
-import ChatContainer from "../components/ChatContainer";
+
+
 import Invoices from "../components/invoices.jsx";
 import Header from "../components/Header.jsx";
 
 const HomePage = () => {
-  const {selectedUser} = useChatStore();
+  
   return (
     <div className="h-screen bg-base-200">
       <div className="flex items-center justify-center pt-20 px-4">
