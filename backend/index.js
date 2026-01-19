0ecfb79bee80561bc0327ffec26b3ddeb76de389
Aditi Baghel <120701036+aditi644@users.noreply.github.com>
2026-01-19 12:08:45 +0530

    Remove unused messageRoutes import
    Removed unused messageRoutes import from index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 
 import express from 'express';
 import authRoutes from './src/routes/auth.route.js'
-import messageRoutes from './src/routes/message.route.js'
+
 import invoiceRoutes from './src/routes/invoices.route.js'
 import dotenv from 'dotenv'
 import { connectDB } from './src/lib/db.js';
