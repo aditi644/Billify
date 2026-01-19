9119d93ecea74f7db563e2878fd6cd61a718b2fb
Aditi Baghel <120701036+aditi644@users.noreply.github.com>
2026-01-19 12:32:27 +0530

    Update SignUpPage.jsx

--- frontend/pages/SignUpPage.jsx
+++ frontend/pages/SignUpPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useAuthStore } from '../src/store/useAuthStore';
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
 import { Link } from "react-router-dom";
-
+import AuthImagePattern from '../components/AuthImagePattern';
 import toast from 'react-hot-toast';
 
 
